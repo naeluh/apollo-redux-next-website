cd6fb41a91bd88a42839e22102db22d310ffd765
Nick Hulea <naeluh@gmail.com>
2019-02-14 11:16:47 -0800

    text

--- pages/_app.js
+++ pages/_app.js
@@ -19,7 +19,6 @@ export default class MyApp extends App {
     return (
       <Container>
         <Head>
-          <style>{`body { margin: 0 } /* custom! */`}</style>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
           <title>My new cool app</title>
