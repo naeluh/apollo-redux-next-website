890313c102f5a597753214fe855a1d2b381deaf2
Nick Hulea <naeluh@gmail.com>
2019-02-14 13:46:32 -0800

    tets

--- pages/_app.js
+++ pages/_app.js
@@ -21,6 +21,7 @@ export default class MyApp extends App {
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
           <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+          <title>My new cool app</title>
         </Head>
         <Component {...pageProps} />
       </Container>
